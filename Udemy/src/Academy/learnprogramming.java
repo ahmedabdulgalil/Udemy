bced316ae89ad7672b6dc1ad29744a944d2b3d6d
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-04-18 13:29:31 -0400

    Primitive Type

---
+++ Udemy/src/Academy/learnprogramming.java
@@ -0,0 +1,19 @@
+package Academy;
+
+public class learnprogramming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int myValue = 1000;
+		int myMinValue = Integer.MIN_VALUE;
+		int myMaxValue = Integer.MAX_VALUE;
+		
+		System.out.println("Integer min value = " + myMinValue);
+		System.out.println("Integer Max value = "+ myMaxValue);
+		System.out.println("Busted Max value = " + (myMaxValue + 1));
+		System.out.println("Busted Max value = "+ (myMinValue - 1));
+
+	}
+
+}
