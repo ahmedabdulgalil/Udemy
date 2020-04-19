152e8d554a3a9bbf69a640771d5cb3ecff81b0da
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-04-19 10:12:57 -0400

    code updated

--- Udemy/src/Academy/learnprogramming.java
+++ Udemy/src/Academy/learnprogramming.java
@@ -4,7 +4,7 @@ public class learnprogramming {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		// Int value
 		int myValue = 1000;
 		int myMinValue = Integer.MIN_VALUE;
 		int myMaxValue = Integer.MAX_VALUE;
@@ -13,6 +13,23 @@ public class learnprogramming {
 		System.out.println("Integer Max value = "+ myMaxValue);
 		System.out.println("Busted Max value = " + (myMaxValue + 1));
 		System.out.println("Busted Max value = "+ (myMinValue - 1));
+		
+		// short value
+		
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		
+		System.out.println("Short Min Value = " + myMinShortValue);
+		System.out.println("Short Max value = " + myMaxShortValue);
+		
+		// Long value 
+		long myLongValue = 100L;
+		
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+		
+		System.out.println(" Long Min Value = " + myMinLongValue);
+		System.out.println(" Long Max value = " + myMaxLongValue);
 
 	}
 
