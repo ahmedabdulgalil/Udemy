90d1a23ff6d3b81f507012d1f695e071e8745e72
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-04-20 22:04:10 -0400

    converted pounds to kilogram

---
+++ Udemy/src/Academy/Challenge.java
@@ -0,0 +1,17 @@
+package Academy;
+
+public class Challenge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		double numberOfPounds = 200d;
+		
+		double convertedKilograms = numberOfPounds * 0.45359237d;;
+		
+		System.out.println(" 200 pounds = " + convertedKilograms +" kilograms");
+
+	}
+
+}
