8873e7680b93b768a841933bc1c08926ece5469d
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-04-20 21:49:20 -0400

    float & double

---
+++ Udemy/src/Academy/FloatAndDouble.java
@@ -0,0 +1,32 @@
+package Academy;
+
+public class FloatAndDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float myMinFloatValue = Float.MIN_VALUE;
+		float myMaxFloatValue = Float.MAX_VALUE;
+		
+		System.out.println("Float Minimum Value = "+ myMinFloatValue);
+		System.out.println("Float Maximum Value = "+ myMaxFloatValue );
+		
+		double myMinDoubleValue = Double.MIN_VALUE;
+		double myMaxDoubleValue = Double.MAX_VALUE;
+		
+		System.out.println("Double Minimum value =  "+ myMinDoubleValue);
+		System.out.println("Double Maximum Value = "+ myMaxDoubleValue);
+		
+		int myIntValue = 5 / 2; 
+		float myFloatValue = 5f / 2; 
+		double myDoubleValue = 5d / 2;
+		
+		System.out.println(" my Int Value = " + myIntValue);
+		System.out.println(" My Float Value = "+ myFloatValue);
+		System.out.println( " My Double Value = "+ myDoubleValue);
+		
+		
+
+	}
+
+}
