c4b03a2d63920f869ec714e250acd1980c65d821
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-04-22 21:34:51 -0400

    Operators

---
+++ Udemy/src/Academy/operators.java
@@ -0,0 +1,39 @@
+package Academy;
+
+public class operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int result = 1 + 2; // 1+3 = 3
+		
+		System.out.println( result);
+		
+		int previousResult = result;
+		
+		System.out.println(" previous Result " + previousResult);
+		result = result - 1;
+		
+		System.out.println(" 3 - 1 =" + result);
+		
+		previousResult = previousResult;
+		System.out.println(" previous Result " + previousResult);
+		
+		result = result * 10;
+		
+		System.out.println(" 2 * 10 = " + result);
+		
+		result = result / 5;
+		
+		System.out.println(" 20  / 5 =  " + result);
+		
+		result = result % 3;
+		
+		System.out.println(" 4 % 3 = " + result);
+		
+		result++;
+		System.out.println(" result"+ result);
+
+	}
+
+}
